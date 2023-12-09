72b98dbf3208845fdaebbf77f3879bd404df1d63
mennamohamed0207 <menamohamed0207@gmail.com>
2023-12-09 22:23:35 +0200

    Shift&add 32 bit with testbench

--- Multiplier2.v
+++ Multiplier2.v
@@ -1,32 +1,32 @@
 module Multiplier2(
-  input [3:0] A, // 4-bit input A
-  input [3:0] B, // 4-bit input B
-  output reg [7:0] P // 8-bit output P
+  input [31:0] A, // 4-bit input A
+  input [31:0] B, // 4-bit input B
+  output reg [63:0] P // 8-bit output P
 );
-reg [3:0] A_signed;
-reg [3:0] B_signed;
+reg [31:0] A_signed;
+reg [31:0] B_signed;
 integer i;
   always @* begin
 
-    if (A[3]==1'b1) begin
-        A_signed[3:0]=~(A[3:0])+1;
+    if (A[31]==1'b1) begin
+        A_signed[31:0]=~(A[31:0])+1;
     end
-    else A_signed[3:0]=A[3:0];
-    if (B[3]==1'b1) begin
-        B_signed[3:0]=~(B[3:0])+1;
+    else A_signed[31:0]=A[31:0];
+    if (B[31]==1'b1) begin
+        B_signed[31:0]=~(B[31:0])+1;
     end
-    else B_signed[3:0]=B[3:0];
-    P = 8'b0; // Initialize P to zero
+    else B_signed[31:0]=B[31:0];
+    P = 64'b0; // Initialize P to zero
     
-    for (i = 0; i < 3; i = i + 1) begin
+    for (i = 0; i < 31; i = i + 1) begin
       P=P<<1;
-      P = P+B_signed[2-i]*A_signed[2:0];
+      P = P+B_signed[30-i]*A_signed[30:0];
     end
 
-    if ((A[3]==1'b1 && B[3]!=1'b1)||(A[3]!=1'b1 && B[3]==1'b1))
-    P[7:0]=~(P[7:0])+1;
-    if(A[3:0]==4'b0000 || B[3:0]==4'b0000)
-    P=8'b00000000;
+    if ((A[31]==1'b1 && B[31]!=1'b1)||(A[31]!=1'b1 && B[31]==1'b1))
+    P[63:0]=~(P[63:0])+1;
+    if(A[31:0]==32'b0 || B[31:0]==32'b0)
+    P=64'b0;
   end
 
 endmodule
