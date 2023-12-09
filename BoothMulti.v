dbd96e84bbb623e24427a3c85c0ea77d12290267
mennamohamed0207 <menamohamed0207@gmail.com>
2023-12-09 15:09:22 +0200

    Booth with 32bits

--- BoothMulti.v
+++ BoothMulti.v
@@ -1,49 +1,43 @@
-
 module Booth (
-    input[3:0] A,
-    input[3:0] B,
-    output reg [7:0] P
+    input[31:0] A,
+    input[31:0] B,
+    output reg [63:0] P
 );
 
-reg [3:0] Q;
+reg [31:0] Q;
 reg Q_1;
-reg [2:0]count;
-wire [3:0]compA;
-reg [3:0] Acc;
-assign compA = ~ (A[3:0]) +1;
+wire [31:0]compA;
+reg [31:0] Acc;
+assign compA = ~ (A[31:0]) +1;
+ integer  count;
 
 always @(A or B) begin
 
- Q[3:0]=B[3:0];
+ Q[31:0]=B[31:0];
 
  //001
  Q_1=0;
- count=3'b100;
- Acc=4'b0000;
- while (count !=3'b000) 
+ Acc=32'b00000000000000000000000000000000;
+ for ( count = 6'b100000; count != 6'b000000; count = count - 1) 
  begin
 
    if (Q_1==0 && Q[0]==1) begin
         Acc=Acc+compA;
    end else begin
     if (Q_1==1 && Q[0]==0) begin
-        Acc=Acc+A[3:0];
+        Acc=Acc+A[31:0];
     end
    end
     //$display("The value before  of  is %b", {Acc,Q,Q_1});
     //$display("The value before  of Acc is %b and Q is %b and Q_1 is %b", Acc,Q,Q_1);
-   {Acc,Q,Q_1}={Acc[3],Acc,Q};
+   {Acc,Q,Q_1}={Acc[31],Acc,Q};
     //$display("The value after of  is %b", {Acc,Q,Q_1});
  //$display("The value after  of Acc is %b and Q is %b and Q_1 is %b", Acc,Q,Q_1);
 
-    count =count-1;
  end
- P[7:0]={ {2{Acc[1]}}, Acc, Q};
+ P[63:0]={ {32{Acc[31]}}, Acc, Q};
 
 end
 
-
-
-    
-
 endmodule
+
--- Booth_tb.v
+++ Booth_tb.v
@@ -1,16 +1,15 @@
-
 `timescale 1 ns / 100 ps
 
 module BoothMultiplier_TB;
 
   // Inputs
-  reg [3:0] A;
-  reg [3:0] B;
+  reg [31:0] A;
+  reg [31:0] B;
 
   // Outputs
-  wire [7:0] P;
+  wire [63:0] P;
 
-  // Instantiate the SerialMultiplier module
+  // Instantiate the Booth module
   Booth uut (
     .A(A),
     .B(B),
@@ -24,76 +23,77 @@ module BoothMultiplier_TB;
   // Test case stimuli
   initial begin
     // Test case 1: 0 * -3 = 0
-    A = 4'b0000;
-    B = 4'b1101;
+    A = 32'b00000000000000000000000000000000;
+    B = 32'b11111111111111111111111111111101;
     #10; // Wait for the result
-    if (P === 8'b00000000)
+    if (P === 64'b0000000000000000000000000000000000000000000000000000000000000000)
       $display("Test Case 1 Passed");
     else
       $display("Test Case 1 Failed");
 
     // Test case 2: 1 * -3 = -3
-    A = 4'b0001;
-    B = 4'b1101;
+    A = 32'b00000000000000000000000000000001;
+    B = 32'b11111111111111111111111111111101;
     #10; // Wait for the result
-    if (P === 8'b11111101)
+    if (P === 64'b1111111111111111111111111111111111111111111111111111111111111101)
       $display("Test Case 2 Passed");
     else
       $display("Test Case 2 Failed");
 
     // Test case 3: -7 * -7 = 49
-    A = 4'b1001;
-    B = 4'b1001;
-    #10; // Wait for the result 0b110001      
-    if (P === 8'b00110001)
+    A = 32'b11111111111111111111111111111001;
+    B = 32'b11111111111111111111111111111001;
+    #10; // Wait for the result
+    if (P === 64'b0000000000000000000000000000000000000000000000000000000000110001)
       $display("Test Case 3 Passed");
     else
       $display("Test Case 3 Failed");
 
-    // Test case 4: -7 * -1 = 7
-    A = 4'b1001;
-    B = 4'b1111;
+    // Test case 4: -7 * 7 = -49
+    A = 32'b11111111111111111111111111111001;
+    B = 32'b00000000000000000000000000000111;
     #10; // Wait for the result
-    if (P === 8'b00000111)
+    if (P === 64'b1111111111111111111111111111111111111111111111111111111111001111)
       $display("Test Case 4 Passed");
     else
       $display("Test Case 4 Failed");
 
-    // Test case 5: -7 * 7 = -49
-    A = 4'b1001;
-    B = 4'b0111;
+    // Test case 5: 0 * 0 = 0
+    A = 32'b00000000000000000000000000000000;
+    B = 32'b00000000000000000000000000000000;
     #10; // Wait for the result
-    if (P === 8'b11001111)
+    if (P === 64'b0000000000000000000000000000000000000000000000000000000000000000)
       $display("Test Case 5 Passed");
     else
       $display("Test Case 5 Failed");
 
-    // Test case 6: 0 * 3 = 0
-    A = 4'b0000;
-    B = 4'b1101;
+    // Test case 6: -1 * -1 = 1
+    A = 32'b11111111111111111111111111111111;
+    B = 32'b11111111111111111111111111111111;
     #10; // Wait for the result
-    if (P === 8'b00000000)
+    if (P === 64'b0000000000000000000000000000000000000000000000000000000000000001)
       $display("Test Case 6 Passed");
     else
       $display("Test Case 6 Failed");
 
-    // Test case 7: -0 * 0 = 0
-    A = 4'b1000;
-    B = 4'b0000;
+    // Test case 7: 1 * 1 = 1
+    A = 32'b00000000000000000000000000000001;
+    B = 32'b00000000000000000000000000000001;
     #10; // Wait for the result
-    if (P === 8'b00000000)
+    if (P === 64'b0000000000000000000000000000000000000000000000000000000000000001)
       $display("Test Case 7 Passed");
     else
       $display("Test Case 7 Failed");
 
-    // Test case 8: 2 * 7 = 14
-    A = 4'b0010;
-    B = 4'b0111;
+    // Test case 8: -1 * 1 = -1
+    A = 32'b11111111111111111111111111111111;
+    B = 32'b00000000000000000000000000000001;
     #10; // Wait for the result
-    if (P === 8'b00001110)
+    if (P === 64'b1111111111111111111111111111111111111111111111111111111111111111)
       $display("Test Case 8 Passed");
     else
       $display("Test Case 8 Failed");
+      
 
   end
 
