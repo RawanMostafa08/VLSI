ef9ed7e8772a1e3cf447b49ae75cbb89f4755c11
RawanMostafa08 <rawanmostafa401@gmail.com>
2023-12-13 00:47:35 +0200

    floating point using plus operator

--- FloatingPointAdder.v
+++ FloatingPointAdder.v
@@ -9,27 +9,18 @@ module FloatingPointAdder (
   reg signed [22:0] result_fraction;
   reg signed [7:0] result_exponent;
   reg result_sign;
-
-  wire signed [22:0] fractionB;
-  wire signed [22:0] fractionA;
-   // Extract components
-  assign sign1 = A[31];
-  assign sign2 = B[31];
-  assign exponent1 = A[30:23];
-  assign exponent2 = B[30:23];
-  assign fraction1 =  A[22:0];
-  assign fractionA =  A[22:0];
-  assign fractionB =  B[22:0];
-  assign fraction2 =  B[22:0];
+ reg [21:0] temp_fraction;
+  integer i;
 
     // Align exponents
-  reg [7:0] exponent_diff;
+  reg signed [7:0] exponent_diff;
   always @* begin
-    //if exp1 --> negative and exp2-->positive 
-    // if(exponent1[7]==1'b1)
-    // exponent1= ~(exponent1)+1;
-    // if(exponent2[7]==1'b1)
-    // exponent2=~(exponent2)+1;
+     sign1 = A[31];
+     sign2 = B[31];
+     exponent1 = A[30:23];
+     exponent2 = B[30:23];
+     fraction1 =  A[22:0];
+     fraction2 =  B[22:0];
     exponent_diff = exponent1 - exponent2;
     if (exponent_diff < 0) begin
       exponent_diff = -exponent_diff;
@@ -41,16 +32,10 @@ module FloatingPointAdder (
       result_exponent = exponent1;
       result_sign = sign1;
     end
-  end
-
-  // Perform addition
-  always @* begin
-    if (sign1 == sign2) begin
+	 
+	 if (sign1 == sign2) begin
       result_fraction = fraction1 + fraction2;
-      // if (result_fraction[23] == 1) begin
-      //   result_fraction = result_fraction >> 1;
-      //   result_exponent = result_exponent + 1;
-      // end
+
     end else begin
       if (fraction1 > fraction2) begin
         result_fraction = fraction1 - fraction2;
@@ -60,20 +45,17 @@ module FloatingPointAdder (
         result_sign = sign2;
       end
     end
-
+	 temp_fraction=result_fraction[21:0];
+    for(i=0;i<22;i=i+1) begin
+    if(temp_fraction[21]==0) begin
+        temp_fraction = temp_fraction <<1;
+        result_exponent =result_exponent-1;
+    end
+    end
+	 
   end
-  // Normalize result
-  // always @* begin
-  //   while (result_fraction[22] == 0 && result_exponent > 0) begin
-  //     result_fraction = result_fraction << 1;
-  //     result_exponent = result_exponent - 1;
-  //   end
-  // end
-
 
   // Construct the result
-  assign result = {result_sign, result_exponent, result_fraction};
+  assign result = {result_sign, result_exponent, result_fraction[22],temp_fraction};
 
 endmodule
-
-
