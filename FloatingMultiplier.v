3abee36b61e87158cff3032e7925893e9f51d382
RawanMostafa08 <rawanmostafa401@gmail.com>
2023-12-24 21:10:42 +0200

    floating multiplier last

--- FloatingMultiplier.v
+++ FloatingMultiplier.v
@@ -6,16 +6,38 @@ module FloatingPointMultiplier (
   reg sign1, sign2;
   reg [7:0] exponent1, exponent2;
   reg  [22:0] fraction1, fraction2;
+  reg  [31:0] fraction1_module,fraction2_module;
   reg signed [45:0] result_fraction;
+  wire signed [63:0] result_fraction_module;
   reg [22:0]  resFrac;
   reg signed [7:0] result_exponent;
   reg result_sign;
   reg [21:0] temp_fraction;
   integer i;
-
-    // Align exponents
+  wire x ;
+  wire cin;
+  assign cin=0;
+  wire cout;
+  reg signed [31:0] exp1,exp2;
+  wire signed [31:0] res;
+assign x="0";
+//instantiation
+MultiplierOperator mult (
+  .A(fraction1_module),
+  .B(fraction2_module),
+  .P(result_fraction_module)
+);
+PlusOperatorAdder add_exponent(
+        .A(exp1),
+        .B(exp2),
+        .Cin(cin),
+        .Sum(res),
+        .Cout(cout)
+      );
+    
   reg signed [7:0] exponent_diff;
   always @* begin
+    //extract parts
      sign1 = A[31];
      sign2 = B[31];
      exponent1 = A[30:23];
@@ -23,14 +45,19 @@ module FloatingPointMultiplier (
      fraction1 =  A[22:0];
      fraction2 =  B[22:0];
 
-
-      result_exponent = exponent1 + exponent2 ;
-      result_fraction = fraction1 * fraction2;
+      fraction1_module={ {9{x}}, fraction1 };
+      fraction2_module={ {9{x}}, fraction2 };
+      exp1={ {24{exponent1[7]}}, exponent1 }; 
+      exp2={ {24{exponent2[7]}}, exponent2 }; 
+      result_exponent =res[7:0];
+      result_fraction=result_fraction_module[45:0];
       resFrac = result_fraction[44:22];
       result_sign = sign1 ^ sign2;
-      
+    
 
 //normalize
+//0 0011 0001
+//0 0001 0100
 //    temp_fraction=result_fraction[21:0];
 	temp_fraction=resFrac[21:0];
     if (resFrac==23'b0) begin
@@ -56,3 +83,4 @@ endmodule
 
 
                                
+
